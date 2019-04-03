10dfc6e81fbbc9f8fe138a5d8604a988db308386
Marco <marcocaaguilar@gmail.com>
2019-04-03 16:00:40 -0600

    Cambio en desarrollo de comisiones

--- modsinter/interkey/models/computedmodels.py
+++ modsinter/interkey/models/computedmodels.py
@@ -92,45 +92,88 @@ class AddComputes(models.Model):
 
 	@api.multi
 	def action_invoice_paid(self):
-		# lots of duplicate calls to action_invoice_paid, so we remove those already paid
-		to_pay_invoices = self.filtered(lambda inv: inv.state != 'paid')
-		if to_pay_invoices.filtered(lambda inv: inv.state not in ('open', 'in_payment')):
-			raise UserError(_('Invoice must be validated in order to set it to register payment.'))
-		if to_pay_invoices.filtered(lambda inv: not inv.reconciled):
-			raise UserError(_('You cannot pay an invoice which is partially paid. You need to reconcile payment entries first.'))
-		for invoice in to_pay_invoices:
-			if any([move.journal_id.post_at_bank_rec and move.state == 'draft' for move in invoice.payment_move_line_ids.mapped('move_id')]):
-				invoice.write({'state': 'in_payment'})
-			else:
-				when_to_pay = self.env['ir.config_parameter'].sudo().get_param('sales_commission_calculation.when_to_pay')
-				if  when_to_pay == 'invoice_payment':
-					for payment in self:
-						commission_based_on = self.env['ir.config_parameter'].sudo().get_param('sales_commission_calculation.commission_based_on')
-						if commission_based_on == 'sales_team':
-							commission_obj = self.env['sales.commission.line']
-							product = self.env['product.product'].search([('is_commission_product','=',1)],limit=1)
-							commission_line = self.env['sales.commission.line'].search([('src_invoice_id', '=', payment.id),],limit=1)
-							if not commission_line:
-								commission = self.env['sales.commission'].search([
-									('commission_user_id', '=', payment.user_id.id),
-									('start_date', '<=', self.date.today()),
-									('end_date', '>=', self.date.today()),
-									('state','=','draft')],limit=1)
-								if not commission:
-									commission = payment.create_base_commission(type='sales_person')
-								calculate = (payment.ganancy * payment.team_id.sales_person_commission)/100
-								if calculate > 0.0:
-									commission_obj = self.env['sales.commission.line']
-									commission_value = {
-									'src_invoice_id': payment.id,
-									'invoice_id': payment.id,
-									'amount': calculate,
-									'origin': payment.name,
-									'type':'sales_person',
-									'product_id': product.id,
-									'date' : self.date.today(),
-									'sales_commission_id':commission.id,
-									}
-									commission_id = commission_obj.create(commission_value)
-				invoice.write({'state': 'paid'})
+		if self.type == 'out_invoice':
+			to_pay_invoices = self.filtered(lambda inv: inv.state != 'paid')
+			if to_pay_invoices.filtered(lambda inv: inv.state not in ('open', 'in_payment')):
+				raise UserError(_('Invoice must be validated in order to set it to register payment.'))
+			if to_pay_invoices.filtered(lambda inv: not inv.reconciled):
+				raise UserError(_('You cannot pay an invoice which is partially paid. You need to reconcile payment entries first.'))
+			for invoice in to_pay_invoices:
+				if any([move.journal_id.post_at_bank_rec and move.state == 'draft' for move in invoice.payment_move_line_ids.mapped('move_id')]):
+					invoice.write({'state': 'in_payment'})
+				else:
+					when_to_pay = self.env['ir.config_parameter'].sudo().get_param('sales_commission_calculation.when_to_pay')
+					if  when_to_pay == 'invoice_payment':
+						for payment in self:
+							commission_based_on = self.env['ir.config_parameter'].sudo().get_param('sales_commission_calculation.commission_based_on')
+							if commission_based_on == 'sales_team':
+								commission_obj = self.env['sales.commission.line']
+								product = self.env['product.product'].search([('is_commission_product','=',1)],limit=1)
+								commission_line = self.env['sales.commission.line'].search([('src_invoice_id', '=', payment.id),],limit=1)
+								if not commission_line:
+									commission = self.env['sales.commission'].search([
+										('commission_user_id', '=', payment.user_id.id),
+										('start_date', '<=', self.date.today()),
+										('end_date', '>=', self.date.today()),
+										('state','=','draft')],limit=1)
+									if not commission:
+										commission = payment.create_base_commission(type='sales_person')
+									calculate = (payment.ganancy * payment.team_id.sales_person_commission)/100
+									if calculate >= 0.0:
+										commission_obj = self.env['sales.commission.line']
+										commission_value = {
+										'src_invoice_id': payment.id,
+										'invoice_id': payment.id,
+										'amount': calculate,
+										'origin': payment.number,
+										'type':'sales_person',
+										'product_id': product.id,
+										'date' : self.date.today(),
+										'sales_commission_id':commission.id,
+										}
+										commission_id = commission_obj.create(commission_value)
+					invoice.write({'state': 'paid'})
+		elif self.type == 'out_refund':
+			to_pay_invoices = self.filtered(lambda inv: inv.state != 'paid')
+			if to_pay_invoices.filtered(lambda inv: inv.state not in ('open', 'in_payment')):
+				raise UserError(_('Invoice must be validated in order to set it to register payment.'))
+			if to_pay_invoices.filtered(lambda inv: not inv.reconciled):
+				raise UserError(_('You cannot pay an invoice which is partially paid. You need to reconcile payment entries first.'))
+			for invoice in to_pay_invoices:
+				if any([move.journal_id.post_at_bank_rec and move.state == 'draft' for move in invoice.payment_move_line_ids.mapped('move_id')]):
+					invoice.write({'state': 'in_payment'})
+				else:
+					when_to_pay = self.env['ir.config_parameter'].sudo().get_param('sales_commission_calculation.when_to_pay')
+					if  when_to_pay == 'invoice_payment':
+						for payment in self:
+							commission_based_on = self.env['ir.config_parameter'].sudo().get_param('sales_commission_calculation.commission_based_on')
+							if commission_based_on == 'sales_team':
+								commission_obj = self.env['sales.commission.line']
+								product = self.env['product.product'].search([('is_commission_product','=',1)],limit=1)
+								commission_line = self.env['sales.commission.line'].search([('src_invoice_id', '=', payment.id),],limit=1)
+								if not commission_line:
+									commission = self.env['sales.commission'].search([
+										('commission_user_id', '=', payment.user_id.id),
+										('start_date', '<=', self.date.today()),
+										('end_date', '>=', self.date.today()),
+										('state','=','draft')],limit=1)
+									if not commission:
+										commission = payment.create_base_commission(type='sales_person')
+									calculate = (payment.ganancy * payment.team_id.sales_person_commission)/100
+									calculatep = calculate * -1
+									if calculatep != 0.0:
+										commission_obj = self.env['sales.commission.line']
+										commission_value = {
+										'src_invoice_id': payment.id,
+										'invoice_id': payment.id,
+										'amount': calculatep,
+										'origin': payment.number,
+										'type':'sales_person',
+										'product_id': product.id,
+										'date' : self.date.today(),
+										'sales_commission_id':commission.id
+										}
+										commission_id = commission_obj.create(commission_value)
+					invoice.write({'state': 'paid'})		
+		
 
